0862670decc4e24512a426cef305623a67127a45
Ransford Asare <ransfordasare2108@gmail.com>
2025-09-02 00:33:08 +0000

    Add files via upload

---
+++ data_cleaning_project.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Load the CSV file (replace with your actual file name)
+df = pd.read_csv(r"C:\Users\Administrator\Desktop\Data Analysis\Data Set For Task\1) iris.csv")
+
+# Show the all rows
+print("display.max_rows", None)
+
+# Check for number of  missing values in columns
+print(df.isnull().sum())
+
+#removing duplicate rows
+df = df.drop_duplicates(keep=False)
+
+#show final dataset
+print(df)
+
+#tabulate dataset
+from tabulate import tabulate
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Administrator\Desktop\Data Analysis\Data Set For Task\1) iris.csv")
